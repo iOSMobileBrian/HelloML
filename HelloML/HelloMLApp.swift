94aa9b2aee0cc14a7b2caee1326c2c0a8f732432
Brian Surface <brian.surface@outlook.com>
2026-03-28 10:41:40 -0400

    Initial Commit

---
+++ HelloML/HelloMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloMLApp.swift
+//  HelloML
+//
+//  Created by Brian Surface on 3/28/26.
+//
+
+import SwiftUI
+
+@main
+struct HelloMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
